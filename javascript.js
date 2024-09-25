ba0f9805298e26ec7db120ff6f1b83bc27574724
Raul Resendez <raul.resendez@proton.me>
2024-09-25 17:01:57 -0500

    All functionality working except for game ending

---
+++ javascript.js
@@ -0,0 +1,191 @@
+function getComputerChoice()
+{
+    let choice = Math.floor(Math.random() * 3);
+    
+    
+    if (choice === 0)
+        {
+            choice = 'rock';
+        }
+        else if (choice === 1)
+            {
+                choice = 'paper';
+            }
+            else
+            {
+                choice = 'scissors';
+            }
+    let computerChoice = choice;
+    return computerChoice;
+};
+
+// console.log(getComputerChoice())
+
+
+function getHumanChoice()
+{
+    selection = prompt("Rock, Paper, or Scissors?");
+    selectionLowerCase = selection.toLowerCase();
+    // console.log(selectionLowerCase);
+    
+
+    while(selectionLowerCase !== 'rock' && selectionLowerCase !== 'paper' && selectionLowerCase !== 'scissors'){
+        console.log("That is not a valid choice. Please make sure you typed it in correctly.")
+        selection = prompt("Rock, Paper, or Scissors?");
+        selectionLowerCase = selection.toLowerCase();
+    }
+
+    let humanChoice = selectionLowerCase;
+
+
+    return humanChoice;
+
+}
+
+
+
+function playRound(humanChoice, computerChoice)
+{
+    if (humanChoice === 'rock')
+        {
+            if (computerChoice === 'paper'){
+                console.log("Player Loses." + ` ${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+                
+            }
+            else if (computerChoice === 'rock')
+                {
+                    console.log(`Tie! Player chose ${humanChoice} and Computer chose ${computerChoice}`);
+                }
+                else {
+                    console.log(`Player wins! ${humanChoice} beats ${computerChoice}`);
+                    humanScore++;
+                }
+        }
+    else if (humanChoice == 'paper')
+        {
+            if (computerChoice === 'paper'){
+                console.log(`Tie! Player chose ${humanChoice} and Computer chose ${computerChoice}`);
+                
+                
+            }
+            else if (computerChoice === 'rock')
+                {
+                    console.log(`Player wins! ${humanChoice} beats ${computerChoice}`);
+                    humanScore++;
+                    
+                }
+                else {
+                    console.log("Player Loses." + ` ${computerChoice} beats ${humanChoice}`);
+                    computerScore++;
+                } 
+
+        }
+        else
+        {
+            if (computerChoice === 'paper'){
+                console.log(`Player wins! ${humanChoice} beats ${computerChoice}`);
+                    humanScore++;
+            }
+            else if (computerChoice === 'rock')
+                {
+                    console.log("Player Loses." + ` ${computerChoice} beats ${humanChoice}`);
+                    computerScore++;
+                }
+                else {
+                    console.log(`Tie! Player chose ${humanChoice} and Computer chose ${computerChoice}`);
+                    
+                } 
+        }
+    
+}
+
+
+
+// playRound(humanSelection, computerSelection);
+
+function playGame()
+{
+    let gamesPlayed =0
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice)
+    {
+        if (humanChoice === 'rock')
+            {
+                if (computerChoice === 'paper'){
+                    console.log("Player Loses." + ` ${computerChoice} beats ${humanChoice}`);
+                    computerScore++;
+                    
+                }
+                else if (computerChoice === 'rock')
+                    {
+                        console.log(`Tie! Player chose ${humanChoice} and Computer chose ${computerChoice}`);
+                    }
+                    else {
+                        console.log(`Player wins! ${humanChoice} beats ${computerChoice}`);
+                        humanScore++;
+                    }
+            }
+        else if (humanChoice == 'paper')
+            {
+                if (computerChoice === 'paper'){
+                    console.log(`Tie! Player chose ${humanChoice} and Computer chose ${computerChoice}`);
+                    
+                    
+                }
+                else if (computerChoice === 'rock')
+                    {
+                        console.log(`Player wins! ${humanChoice} beats ${computerChoice}`);
+                        humanScore++;
+                        
+                    }
+                    else {
+                        console.log("Player Loses." + ` ${computerChoice} beats ${humanChoice}`);
+                        computerScore++;
+                    } 
+
+            }
+            else
+            {
+                if (computerChoice === 'paper'){
+                    console.log(`Player wins! ${humanChoice} beats ${computerChoice}`);
+                        humanScore++;
+                }
+                else if (computerChoice === 'rock')
+                    {
+                        console.log("Player Loses." + ` ${computerChoice} beats ${humanChoice}`);
+                        computerScore++;
+                    }
+                    else {
+                        console.log(`Tie! Player chose ${humanChoice} and Computer chose ${computerChoice}`);
+                        
+                    } 
+            }
+        
+    }
+    while(gamesPlayed < 5)
+        {
+            const humanSelection = getHumanChoice();
+            const computerSelection = getComputerChoice();
+
+            playRound(humanSelection, computerSelection)
+            console.log(`Player Score: ${humanScore}   Computer Score: ${computerScore}`);
+
+        }
+    
+    if (humanScore > computerScore){
+        console.log("You Win!!!!!!");
+    }
+    else if (humanScore === computerScore)
+        {
+            console.log("You tie with computer");
+        }
+        else{
+            console.log("You lose!!")
+        }
+
+}
+
+playGame();
+
